50dd0b0d1771b4b1590d804a56abb4748af249ac
Malcolm Strong <strong.malcolm@yahoo.com>
2017-06-03 13:29:20 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+var startYear = $("#year1").val() + "0101";
+var endYear = $("#year2").val() + "1231";
+var numberOfrecords = $("#records").val();
+var subject = $("#term").val();
+
+var articleCounter = 0;
+
+var numArticles ;
+
+
+ function runThis(numArticles) {}
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "6c06af0cde254bc0a14d82aaa261021c" ,
+  'q': subject ,
+  'begin_date': startYear ,
+  'end_date': endYear ,
+
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+
+  for (var i = 0; i < numArticles; i++) {
+
+      articleCounter++;
+
+      var wellSection = $("<div>");
+      wellSection.addClass("well");
+      wellSection.attr("id", "article-well-" + articleCounter);
+      $("#well-section").append(wellSection);
+}
+
+
+
+
+}).fail(function(err) {
+  throw err;
+});
